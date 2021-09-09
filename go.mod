83486e060545f93c2f12765e0b63dfffae645a7e
rpajay <ajay@in2tive.ai>
2021-09-09 16:35:45 +0530

    New relic APM integration

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/newrelic/go-agent/v3 v3.15.0
 	go.elastic.co/apm/module/apmgorilla v1.13.1
 )
--- main.go
+++ main.go
@@ -5,23 +5,33 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"go.elastic.co/apm/module/apmgorilla"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("hello")
 	fmt.Fprintf(w, `{"route":"/hello","response":"hello world v3 branch"}`)
 }
 
 func empty(w http.ResponseWriter, req *http.Request) {
-
+	fmt.Println("empty")
 	fmt.Fprintf(w, `{"route":"/","branch":"v3"}`)
 }
 
 func main() {
+
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigFromEnvironment(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.Use(apmgorilla.Middleware())
 
-	r.HandleFunc("/", empty)
-	r.HandleFunc("/hello", hello)
-	fmt.Print(http.ListenAndServe(":3000", r))
+	r.HandleFunc(newrelic.WrapHandleFunc(app, "/", empty))
+	r.HandleFunc(newrelic.WrapHandleFunc(app, "/hello", hello))
+	fmt.Print(http.ListenAndServe(":30100", r))
 }
